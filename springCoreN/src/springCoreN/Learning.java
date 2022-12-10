9c98a89e50aa9e8b21ac1301771dbe3af5550320
mvikaskumar <vikaskricot@gmail.com>
2022-12-11 00:06:25 +0530

    Learning Tomorrow

---
+++ springCoreN/src/springCoreN/Learning.java
@@ -0,0 +1,11 @@
+package springCoreN;
+
+public class Learning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I Will Start learning spring core from tomorrow.");
+
+	}
+
+}
