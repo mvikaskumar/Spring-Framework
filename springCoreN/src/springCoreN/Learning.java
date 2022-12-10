2201db54c3416bc3a8b031f5047e3dca0d03041f
mvikaskumar <vikaskricot@gmail.com>
2022-12-11 00:12:22 +0530

    2nd commit fun

--- springCoreN/src/springCoreN/Learning.java
+++ springCoreN/src/springCoreN/Learning.java
@@ -5,6 +5,7 @@ public class Learning {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I Will Start learning spring core from tomorrow.");
+		System.out.println("Most probablly I take months in doing this. But finnaly I'm happy that \n i got start with this. This will make my work eaiser and funn also.");
 
 	}
 
